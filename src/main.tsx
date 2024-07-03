e47eb2788926e2f76fb888fa4cfe40b42a0f61b6
unknown <mirlanmamaraimov271@gmail.com>
2024-07-03 19:24:39 +0600

    create-Theme-finish

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app/App";
+import { theme } from "./shader/theme/FeaturesTheme";
+import { ThemeProvider } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>
 );
