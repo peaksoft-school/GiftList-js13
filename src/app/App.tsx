c7231e6c4fa7b0167e6565bfc6f0ecbaf4b112ae
rahatbekdev <rahatbekmamyrbaev103@gmail.com>
2024-07-03 18:27:05 +0600

    create button,add props

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,29 @@
+import { MyButton } from "../shader/UI/button/Button";
+
 function App() {
-  return <></>;
+  return (
+    <>
+    <br />
+      <div>
+        <MyButton variant="text">войти</MyButton>
+        <MyButton variant="contained">добавить подарок</MyButton>
+        <MyButton variant="outlined">отмена</MyButton>
+      </div>
+      <br />
+      <div>
+        <MyButton variant="text" disabled>
+          войти
+        </MyButton>
+        <MyButton variant="contained" disabled>
+          добавить подарок
+        </MyButton>
+        <MyButton variant="outlined" disabled>
+          отмена
+        </MyButton>
+      </div>
+      <MyButton  variant="text">jjh b</MyButton>
+    </>
+  );
 }
 
 export default App;
