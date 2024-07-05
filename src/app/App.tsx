3489ca5c8323dbec26a3091b43189bc17b31e817
Syrgabek <syrgabeka766@gmail.com>
2024-07-05 13:07:40 +0600

    fixsedInput

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,5 @@
-import { Input } from "../shader/UI/input/Input";
-
 function App() {
-  return (
-    <>
-      <div style={{ width: "50%", margin: "20px " }}>
-        <Input
-          placeholder="Введите название праздника"
-          label="Название праздника"
-          helperText="ошибка"
-          id="1"
-        />
-        <Input
-          placeholder="Введите название праздника"
-          label="Название праздника"
-          helperText="ошибка"
-          id="1"
-          error={true}
-        />
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
