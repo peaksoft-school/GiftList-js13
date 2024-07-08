b1eb58f805f70cce57b60026903647316a791d1f
Syrgabek <syrgabeka766@gmail.com>
2024-07-09 00:34:31 +0600

    fixet createSearchInput

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-
-function App() {
-  return (
-    <>
-    </>
-  );
-=======
 function App() {
   return <></>;
-
->>>>>>> e5d67eb91eb9d1560192d88d952883979f171bc5
 }
 
 export default App;
