23edcda7f8ae5d375852dc93b312de6b0f5710b0
rahatbekdev <rahatbekmamyrbaev103@gmail.com>
2024-07-11 11:48:59 +0600

    FriendsCard

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,5 @@
-import FriendsCard from "../entities/card/FriendsCard";
-const arr = [
-  {
-    id: 1,
-    name: "Joe Block",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDR8H0rgV-zmSodkT_erGjzA_VhfWE22Pg7Q&s",
-    counter: 89,
-    wish: "Frast",
-    counterTwo: 99,
-    holiday: "KJBKJBKJB",
-  },
-];
 function App() {
-  return (
-    <>
-      <FriendsCard arrow={arr} />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
