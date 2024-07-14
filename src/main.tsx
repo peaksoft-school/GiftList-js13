de0e19b0a6467bf18e309bef262045b66523d3b3
rahatbekdev <rahatbekmamyrbaev103@gmail.com>
2024-07-14 11:50:08 +0600

    create new toast

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app/App";
-// import { ThemeProvider } from "@mui/material";
-// import { theme } from "./shader/theme/featureThemes";
-// import { LocalizationProvider } from "@mui/x-date-pickers";
-// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./shader/theme/featureThemes";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		{/* <LocalizationProvider dateAdapter={AdapterDayjs}> */}
-			{/* <ThemeProvider theme={theme}> */}
-				<App />
-			{/* </ThemeProvider> */}
-		{/* </LocalizationProvider> */}
-	</React.StrictMode>
+  <React.StrictMode>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </LocalizationProvider>
+  </React.StrictMode>
 );
--- src/shader/UI/toast/Toast.tsx
+++ src/shader/UI/toast/Toast.tsx
@@ -1,83 +1,26 @@
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { styled } from "@mui/material/styles";
-import { IconButton } from "@mui/material";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-import CloseIcon from "@mui/icons-material/Close";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const CustomToastContainer = styled(ToastContainer)(({ theme }) => ({
-  width: "500px",
+type ToastType = 'info' | 'success' | 'warning' | 'error';
 
-  ".Toastify__toast--success": {
-    background: "#EAFBE7",
-    border: "1px solid rgba(234, 251, 231, 1)",
-    borderRadius: "4px",
-    height: "107px",
-    color: theme.palette.text.primary,
-  },
-}));
-
-const ToastComponent = () => {
-  const notify = () => {
-    toast(
-      <ToastMessage>
-        <Header>
-          <CheckIcon />
-          <BoldText>Спасибо что сообщили нам об этом</BoldText>
-          <IconButton
-            onClick={() => toast.dismiss()}
-            style={{ color: "inherit", marginLeft: "auto" }}
-          >
-            <CloseIcon />
-          </IconButton>
-        </Header>
-        <RegularText>
-          Ваши отзывы помогают нам сделать сообщество GIFT LIST безопасной
-          средой для всех.
-        </RegularText>
-      </ToastMessage>,
-      {
-        closeButton: false,
-      }
-    );
-  };
-
-  return (
-    <div>
-      <button onClick={notify}>Показать Toast</button>
-      <CustomToastContainer />
-    </div>
-  );
+const showToast = (type: ToastType, message: string) => {
+  switch (type) {
+    case 'info':
+      toast.info(message);
+      break;
+    case 'success':
+      toast.success(message);
+      break;
+    case 'warning':
+      toast.warning(message);
+      break;
+    case 'error':
+      toast.error(message);
+      break;
+    default:
+      toast(message);
+      break;
+  }
 };
 
-const ToastMessage = styled("div")({
-  display: "flex",
-  flexDirection: "column",
-  padding: "10px",
-  position: "relative",
-});
-
-const Header = styled("div")({
-  display: "flex",
-  alignItems: "center",
-  paddingBottom: "1px",
-});
-
-const CheckIcon = styled(CheckCircleIcon)({
-  color: "rgba(50, 128, 72, 1)",
-  width: "20px",
-  height: "20px",
-});
-
-const BoldText = styled("p")({
-  fontWeight: "bold",
-  fontSize: "14px",
-  color: "rgba(50, 50, 77, 1)",
-});
-
-const RegularText = styled("p")({
-  fontSize: "14px",
-  color: "rgba(50, 50, 77, 1)",
-});
-
-export default ToastComponent;
+export default showToast;
