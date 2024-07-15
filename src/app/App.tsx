3a52c8a9199651ca6129c2d2dbad2a8f5e2d990e
rahatbekdev <rahatbekmamyrbaev103@gmail.com>
2024-07-15 17:17:41 +0600

    delete app elements

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,36 +1,8 @@
-// function App() {
-//   return <></>;
-// }
-// export default App;
-
-// src/App.tsx
-
-// src/App.tsx
-import React from "react";
-import { Button } from "@mui/material";
-import Toast from "../shader/toast/Toast";
-import { showToast } from "../shader/toast/toastType";
-
-
-const App: React.FC = () => {
-  const handleShowSuccessToast = () => {
-    showToast({
-      type: "success",
-      title: "Спасибо что сообщили нам об этом",
-      message: "Ваши отзывы помогают нам сделать сообщество GIFT LIST безопасной средой для всех."
-    });
-  };
-
-  return (
-    <div className="App">
-      <h1>Пример использования Toast</h1>
-      <Button variant="contained" onClick={handleShowSuccessToast}>
-        Показать успешный Toast
-      </Button>
-      <Toast />
-    </div>
-  );
-};
-
+function App() {
+  return <>
+  <h1>Hello</h1>
+  </>;
+}
 export default App;
+
 
