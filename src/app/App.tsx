7ce8761ddb7f0dce40103b279a9e6a01cbe0126e
Syrgabek <syrgabeka766@gmail.com>
2024-07-18 19:36:25 +0600

    app delete

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
-import LandingHeader from "../widgets/header/LandingHeader";
 
-const App: FC = () => <LandingHeader />;
+const App: FC = () => <h1>GIFT LIST_13</h1>;
 
 export default App;
