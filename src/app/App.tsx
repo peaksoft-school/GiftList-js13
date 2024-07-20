08928136809d707d4e725d5e1f3fdcad4713d941
rahatbekdev <rahatbekmamyrbaev103@gmail.com>
2024-07-20 11:53:47 +0600

    save

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,7 @@
 import { FC } from "react";
-import  LandingAboutProject  from "../widgets/landimg/landingAboutProject/LandingAboutProject";
 
 const App: FC = () => <>
-{/* <h1>GIFT LIST_13</h1> */}
-<LandingAboutProject/>
+<h1>GIFT LIST_13</h1>
 </>;
 
 export default App;
