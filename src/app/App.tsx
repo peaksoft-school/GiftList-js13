bfe7a969e22efb2d7921b5d88603b8f2fb313690
Syrgabek <syrgabeka766@gmail.com>
2024-07-24 17:22:03 +0600

    change app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,5 @@
 import { FC } from "react";
-import Meatballs from "../shared/UI/Meatballs";
 
-const data = [
-  {
-    id: 1,
-    title: "title",
-    icon: "src/assets/icon/anonim.svg",
-    onClick: () => {
-      console.log("asd");
-    },
-  },
-];
-
-const App: FC = () => (
-  <>
-    <Meatballs data={data} />
-  </>
-);
+const App: FC = () => <h1>GIFT LIST_13</h1>;
 
 export default App;
