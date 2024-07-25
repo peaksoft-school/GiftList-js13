08f349d27820b5c21b743462a1aa3cb5f659c1da
Syrgabek <syrgabeka766@gmail.com>
2024-07-25 13:06:52 +0600

    delete components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
-import { AppRouter } from "./router/AppRouter";
 
-const App: FC = () => <AppRouter />;
+const App: FC = () => <h1>GIFT LIST_13</h1>;
 
 export default App;
--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -1,49 +1,5 @@
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
-import BaseLayout from "../../pages/components/Baselayout";
-import Courses from "../../pages/components/Courses";
-import Material from "../../pages/components/Material";
-import Anouncements from "../../pages/components/Anouncements";
-import Students, { All, MyGroup } from "../../pages/components/Students";
-
-export const AppRouter = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <BaseLayout />,
-      children: [
-        { index: true, element: <Navigate to={"courses/material"} /> },
-        {
-          path: "courses",
-          element: <Courses />,
-          children: [
-            { index: true, element: <Navigate to={"material"} /> },
-
-            {
-              path: "material",
-              element: <Material />,
-            },
-            {
-              path: "students",
-              element: <Students />,
-              children: [
-                { index: true, element: <Navigate to="all" /> },
-                { path: "all", element: <All /> },
-                { path: "myGroup", element: <MyGroup /> },
-              ],
-            },
-          ],
-        },
-        {
-          path: "anouncements",
-          element: <Anouncements />,
-        },
-      ],
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+const AppRouter = () => {
+  return <></>;
 };
+
+export default AppRouter;
