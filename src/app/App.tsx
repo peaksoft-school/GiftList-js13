fc8f6339885fee9ec132f4cea7575f875e6310b0
rahatbekdev <rahatbekmamyrbaev103@gmail.com>
2024-08-12 11:02:16 +0600

    user profile

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,9 @@
-// import { FC } from "react";
-
-// const App: FC = () => <h1>GIFT LIST_13</h1>;
-
-// export default App;
-
-
 import { FC } from "react";
-import ProfilePage from "../pages/ProfilePage";
 
-const App: FC = () => <><ProfilePage/></>;
+const App: FC = () => <h1>GIFT LIST_13</h1>;
 
 export default App;
+
+
+
 
