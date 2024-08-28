11c78f07eae8ee9979242e24d351a313a164d009
rahatbekdev <rahatbekmamyrbaev103@gmail.com>
2024-08-28 16:11:09 +0600

    save

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,8 @@
-// import { FC } from "react";
-
-// const App: FC = () => <h1>GIFT LIST_13</h1>;
-
-// export default App;
-
 import { FC } from "react";
-import AppRouter from "./router/AppRouter";
-import NewsLetters from "../shared/newsLetters/NewsLetters";
 
-const App: FC = () => (
-  <>
-    <AppRouter />
-    <NewsLetters />
-  </>
-);
+const App: FC = () => <h1>GIFT LIST_13</h1>;
 
 export default App;
+
+
+
